8e594f3fd1f163d8212b2ef1bbc0f20ba7c8ecfd
Jane Charlotte Lange <jlange20@barrett2.stanford.edu>
2018-07-16 15:57:03 -0700

    .

--- ddSMT-tests/issue1349/script.sh
+++ ddSMT-tests/issue1349/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-./../../ddsmt.py -vv -o "" -r  -t 10 --round 30 ./DRAGON_11_e3_382_e1_505.lus.ic3.1.smt2 ./edit-version2.min.smt2 ./DRAGON_11_e3_382_e1_505.lus.ic3.1.sh
-#./../../ddsmt.py -vv -o -t 1 ./DRAGON_14.lus.bmc.1.smt2 ./edit-version.min.smt2 ./DRAGON_14.lus.bmc.1.sh
+#./../../ddsmt.py -vv -o "" -r  -t 10 --round 30 ./DRAGON_11_e3_382_e1_505.lus.ic3.1.smt2 ./edit-version2.min.smt2 ./DRAGON_11_e3_382_e1_505.lus.ic3.1.sh
+#./../../ddsmt.py -vv -o "" -t 1 ./DRAGON_14.lus.bmc.1.smt2 ./edit-version.min.smt2 ./DRAGON_14.lus.bmc.1.sh
+./../../ddsmt.py -vv -o "" -t 1 ./DRAGON_14.lus.ic3.1.smt2 ./edit-version3.min.smt2 ./DRAGON_14.lus.ic3.1.sh
 
 #vim ./edit-version.min.smt2
