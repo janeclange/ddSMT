3b5104d0e8b14124132b27c956e255a2c08d8626
Jane Charlotte Lange <jlange20@barrett2.stanford.edu>
2018-08-15 11:53:39 -0700

    removed redundant lines

--- ddSMT-tests/issue1349/script.sh
+++ ddSMT-tests/issue1349/script.sh
@@ -2,7 +2,7 @@
 
 #./../../hddsmt.py -v -o "" -t 3 --rel ./DRAGON_11_e3_382_e1_505.lus.ic3.1.smt2 ./edit-version2.min.smt2 ./DRAGON_11_e3_382_e1_505.lus.ic3.1.sh
 #./../../hddsmt.py -v -o "" -t 3 --rel ./edit-version3.min.smt2 ./edit-version2.min.smt2 ./DRAGON_11_e3_382_e1_505.lus.ic3.1.sh
-#./../../ddportfolio.py -v -o "" ./DRAGON_14.lus.bmc.1.smt2 ./edit-version.min.smt2 ./DRAGON_14.lus.bmc.1.sh
-./../../hddsmt.py -vv -o "" --round 20 ./DRAGON_14.lus.ic3.1.smt2 ./edit-version3.min.smt2 ./DRAGON_14.lus.ic3.1.sh
+./../../hddsmt.py -v -o "" ./DRAGON_14.lus.bmc.1.smt2 ./edit-version.min.smt2 ./DRAGON_14.lus.bmc.1.sh
+#./../../hddsmt.py -vv -o "" --round 20 ./DRAGON_14.lus.ic3.1.smt2 ./edit-version3.min.smt2 ./DRAGON_14.lus.ic3.1.sh
 
 #vim ./edit-version.min.smt2
